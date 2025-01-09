a35f788740cffb08bc9c983a1373646f2866f37c
F72I9D1 <marney.pivetta@fiserv.com>
2025-01-09 19:37:11 -0300

    ajustando autenticacao

--- src/main/java/com/fiserv/dummy_transaction_api/outbound/auth/KeycloakConfig.java
+++ src/main/java/com/fiserv/dummy_transaction_api/outbound/auth/KeycloakConfig.java
@@ -1,38 +1,40 @@
 package com.fiserv.dummy_transaction_api.outbound.auth;
 
-import jakarta.servlet.http.HttpServletResponse;
-import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+import static org.springframework.security.config.Customizer.withDefaults;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.session.NullAuthenticatedSessionStrategy;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 
 @Configuration
+@EnableWebSecurity
 public class KeycloakConfig {
 
 	@Bean
-	public KeycloakSpringBootConfigResolver configResolver() {
-		return new KeycloakSpringBootConfigResolver();
+	protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+		return new NullAuthenticatedSessionStrategy();
 	}
 
 	@Bean
-	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		return http
-				.csrf(csrf -> csrf.disable())
+				.oauth2Login(withDefaults())
+				.csrf(AbstractHttpConfigurer::disable)
+				.sessionManagement(sessionManagement ->
+									sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 				.authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
-				.httpBasic(Customizer.withDefaults())
-				.oauth2ResourceServer(oauth2 -> oauth2
-						.jwt(jwt -> jwt.jwtAuthenticationConverter(getJwtAuthenticationConverter()))
-				)
+				.oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(converter())))
 				.build();
 	}
 
-	private JwtAuthenticationConverter getJwtAuthenticationConverter() {
-		JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
-		return converter;
+	private JwtAuthenticationConverter converter() {
+		return new JwtAuthenticationConverter();
 	}
-
-
 }
